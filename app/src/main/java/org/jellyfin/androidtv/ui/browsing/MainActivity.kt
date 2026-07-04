f8c0071cd903271d538d5cadc8517026517aec2e
Aanish Farrukh <249387242+sudoaanish@users.noreply.github.com>
2026-07-03 23:23:22 -0400

    Fix Toast import in MainActivity

--- app/src/main/java/org/jellyfin/androidtv/ui/browsing/MainActivity.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/browsing/MainActivity.kt
@@ -2,6 +2,7 @@ package org.jellyfin.androidtv.ui.browsing
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager
