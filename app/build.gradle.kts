29108bf421f9271b58886ae16832cc986dd38bed
Aanish Farrukh <249387242+sudoaanish@users.noreply.github.com>
2026-07-04 03:44:53 -0400

    Prepare v0.2.4 signed rescue release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 
 		// Release version
 		applicationId = "com.pakflix.tv"
-		versionName = "0.2.3"
-		versionCode = 203
+		versionName = "0.2.4"
+		versionCode = 204
 	}
 
 	buildFeatures {
