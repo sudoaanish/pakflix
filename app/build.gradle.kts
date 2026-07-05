903750a8b4782c2b542187b6343a613fb2e82a25
Aanish Farrukh <249387242+sudoaanish@users.noreply.github.com>
2026-07-05 11:18:05 -0400

    Prepare v0.2.10 updater test release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 
 		// Release version
 		applicationId = "com.pakflix.tv"
-		versionName = "0.2.9"
-		versionCode = 209
+		versionName = "0.2.10"
+		versionCode = 210
 	}
 
 	buildFeatures {
