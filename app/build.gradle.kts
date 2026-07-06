f1db4875fabca560b449396797ff19bb994fb8e4
Aanish Farrukh <249387242+sudoaanish@users.noreply.github.com>
2026-07-06 05:36:28 -0400

    Prepare v0.3.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 
 		// Release version
 		applicationId = "com.pakflix.tv"
-		versionName = "0.2.14"
-		versionCode = 214
+		versionName = "0.3.0"
+		versionCode = 300
 	}
 
 	buildFeatures {
